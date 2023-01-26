44e1b3a2bc838222ba91e69944f120ad1ec3adbf
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-26 08:11:49 +0530

    Add files via upload

---
+++ Squats.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,s;
+        scanf("%d",&x);
+        s=x*15;
+        printf("%d\n",s);
+    }
+}
