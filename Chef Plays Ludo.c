35803924aa3e1fdbdb52bb5266388011ce7ba7a4
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-27 22:43:59 +0530

    Add files via upload

---
+++ Chef Plays Ludo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        if(n==6)
+        printf("Yes\n");
+        else
+        printf("No\n");
+    }
+}
