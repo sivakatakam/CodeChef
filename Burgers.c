f310fe07b7e195375c985157155f75058e5a5f7f
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-27 22:42:57 +0530

    Add files via upload

---
+++ Burgers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d%d",&a,&b);
+        if(a<b)
+        printf("%d\n",a);
+        else
+        printf("%d\n",b);
+    }
+}
