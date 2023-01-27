8d2814679df432054752219f9c74b8e02a2d705d
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-27 22:48:00 +0530

    Add files via upload

---
+++ Saving Taxes.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y,a;
+        scanf("%d%d",&x,&y);
+        if(x>y)
+        {
+            a=x-y;
+            printf("%d\n",a);
+        }
+    }
+}
