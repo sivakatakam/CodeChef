41d42f12bc2ecc3301c0d489ebd4018ce361ea55
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-27 22:45:58 +0530

    Add files via upload

---
+++ Fitness.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int k,m;
+        scanf("%d",&k);
+        m=2*k*5;
+        printf("%d\n",m);
+    }
+}
