ab84ae10c19b70a59ee810ad94542d2302d83410
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-27 22:42:14 +0530

    Add files via upload

---
+++ Biriyani Classes.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,m;
+        scanf("%d%d",&a,&b);
+        m=a*b;
+        printf("%d\n",m);
+    }
+}
