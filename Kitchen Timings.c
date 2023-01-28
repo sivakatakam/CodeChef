95cc6860c14c5187eaa7871475e01272a97e9f41
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-28 10:29:09 +0530

    Add files via upload

---
+++ Kitchen Timings.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y,a;
+        scanf("%d%d",&x,&y);
+        a=y-x;
+        printf("%d\n",a);
+    }
+}
