b133b0a062ab16b9d802bf7e6379689bb698d7c1
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-28 10:28:43 +0530

    Add files via upload

---
+++ Determine the Score.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y,a;
+        scanf("%d%d",&x,&y);
+        a=(x/10)*y;
+        printf("%d\n",a);
+    }
+}
