b2498dbed21dcb1ef083f86ebb918816bd6c4749
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-29 08:13:42 +0530

    Add files via upload

---
+++ Who is Taller!.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        if(x>y)
+        printf("A\n");
+        else
+        printf("B\n");
+    }
+}
