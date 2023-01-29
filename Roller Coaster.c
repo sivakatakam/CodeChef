92ec0760c44745052ef936de3099661155817b8c
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-01-30 01:41:59 +0530

    Add files via upload

---
+++ Roller Coaster.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,h;
+        scanf("%d%d",&x,&h);
+        if(x>=h)
+        printf("YES\n");
+        else
+        printf("NO\n");
+    }
+}
