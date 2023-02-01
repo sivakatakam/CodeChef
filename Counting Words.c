008f7488eba29d8155b6fd22a939574ad2d278be
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-01 07:43:48 +0530

    Add files via upload

---
+++ Counting Words.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,m,a;
+        scanf("%d%d",&n,&m);
+        a=n*m;
+        printf("%d\n",a);
+    }
+}
