cf38bde2659f26cbe767b9f9cd37439955ef3e6b
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-01 07:44:09 +0530

    Add files via upload

---
+++ Total Prize Money.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+
+    {
+        int x,y,a;
+        scanf("%d%d",&x,&y);
+        a=x*10+y*90;
+        printf("%d\n",a);
+    }
+}
