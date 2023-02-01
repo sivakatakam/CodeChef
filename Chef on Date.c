428b2d66bb0c970dd1aed709fbba1bc1dfda8026
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-01 07:43:05 +0530

    Add files via upload

---
+++ Chef on Date.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        if(y<=x)
+        printf("YES\n");
+        else
+        printf("NO\n");
+    }
+}
