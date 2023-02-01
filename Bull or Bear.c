6d190bc3740e8ff4f5a4b800415de4ff3e933990
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-01 07:44:42 +0530

    Add files via upload

---
+++ Bull or Bear.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        if(x>y)
+        printf("LOSS\n");
+        else if(x<y)
+        printf("PROFIT\n");
+        else
+        printf("NEUTRAL\n");
+    }
+}
