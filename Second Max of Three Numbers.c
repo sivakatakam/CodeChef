806a8dc941e1b36dd6cbb4899f66558dd9e0a3e4
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-02 06:20:17 +0530

    Add files via upload

---
+++ Second Max of Three Numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n--)
+    {
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        if(a>b&&b>c)
+        printf("%d\n",b);
+        else if(b>c&&c>a)
+        printf("%d\n",c);
+        else if(a>c&&c>b)
+        printf("%d\n",c);
+        else if(b>a&&a>c)
+        printf("%d\n",a);
+        else if(c>a&&a>b)
+        printf("%d\n",a);
+        else if(c>b&&b>a)
+        printf("%d\n",b);
+    }
+}
