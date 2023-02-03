c3f3df54eda0c0e8ab68089f70d700a854fa5978
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-03 06:14:12 +0530

    Add files via upload

---
+++ Enough Space.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,x,y;
+        scanf("%d%d%d",&n,&x,&y);
+        if(n>=x+y*2)
+        printf("YES\n");
+        else
+        printf("NO\n");
+    }
+    
+}
