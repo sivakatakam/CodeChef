96172c75ac33898b1b782b90e5eda45d0f4a169d
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-04 19:40:26 +0530

    Add files via upload

---
+++ Bidding.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        if(a>b&&a>c)
+        printf("Alice\n");
+        else if(b>a&&b>c)
+        printf("Bob\n");
+        else
+        printf("Charlie\n");
+    }
+}
