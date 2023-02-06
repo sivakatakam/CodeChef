63562dda408751820224ccd251b182f14eec6487
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-06 19:37:52 +0530

    Add files via upload

---
+++ Fever.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x>98)
+        printf("YES\n");
+        else
+        printf("NO\n");
+    }
+}
