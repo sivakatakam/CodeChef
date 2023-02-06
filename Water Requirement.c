7a373ade43affd1b8e49015f7b0f5917c2a9a2e9
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-06 20:00:05 +0530

    Add files via upload

---
+++ Water Requirement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,a;
+        scanf("%d",&n);
+        a=n*2;
+        printf("%d\n",a);
+    }
+}
