a4b24cc21b7f197be1c0daedf8d0318aed4f05c8
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-09 09:11:49 +0530

    Add files via upload

---
+++ Battery Health.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x>=80)
+        printf("YES\n");
+        else
+        printf("NO\n");
+    }
+}
