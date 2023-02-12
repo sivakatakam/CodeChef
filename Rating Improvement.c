f014ccefe40623f8ccdea60fcd57a658ae4c0f7f
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-12 10:12:02 +0530

    Add files via upload

---
+++ Rating Improvement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        if(y>=x&&y<=x+200)
+        printf("YES\n");
+        else
+        printf("NO\n");
+    }
+}
