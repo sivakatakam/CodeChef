1c5905c614882c7a884db3e2780c1b09344d9d71
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-15 18:35:55 +0530

    Add files via upload

---
+++ Fill the Bucket.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int k,x,a;
+        scanf("%d%d",&k,&x);
+        a=k-x;
+        printf("%d\n",a);
+    }
+}
