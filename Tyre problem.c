23e6865ad9c83b37773e23eba676116f0ffee075
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-17 17:32:31 +0530

    Add files via upload

---
+++ Tyre problem.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,y,t;
+        scanf("%d%d",&x,&y);
+        t=2*x+4*y;
+        printf("%d\n",t);
+    }
+}
