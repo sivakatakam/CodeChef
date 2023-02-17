7bd57324d2d3ea28d4737df3da2f76a4bb04bd0a
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-17 17:30:41 +0530

    Add files via upload

---
+++ Maximum Submissions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,s;
+        scanf("%d",&x);
+        s=2*x;
+        printf("%d\n",s);
+    }
+}
