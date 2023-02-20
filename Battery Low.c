a06ef2925881dab989be612bb62a36be7f4f04dc
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-20 20:54:19 +0530

    Add files via upload

---
+++ Battery Low.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x;
+        scanf("%d",&x);
+        if(x>15)
+        printf("NO\n");
+        else
+        printf("YES\n");
+    }
+}
