8ab12d41cc1a2b61c4ecf1249dd654fdd2199318
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-23 13:49:17 +0530

    Add files via upload

---
+++ Shopping Change.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,a;
+        scanf("%d",&x);
+        a=100-x;
+        printf("%d\n",a);
+    }
+}
