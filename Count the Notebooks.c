6e07975529d5aa0ae4eebf972b54539c36d3c518
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-26 09:04:47 +0530

    Add files via upload

---
+++ Count the Notebooks.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,a;
+        scanf("%d",&n);
+        a=n*10;
+        printf("%d\n",a);
+    }
+}
