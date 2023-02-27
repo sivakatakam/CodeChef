6e77bab66f198044d9de000dd1a8a1b7dac473fd
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-02-27 20:43:52 +0530

    Add files via upload

---
+++ Is the Score Consistent.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,c,d;
+        scanf("%d%d%d%d",&a,&b,&c,&d);
+        if(a<=c&&b<=d)
+        printf("POSSIBLE\n");
+        else
+        printf("IMPOSSIBLE\n");
+    }
+}
