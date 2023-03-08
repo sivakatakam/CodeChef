88524309aaf12191c7b796c86117d6ced65545d5
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-03-08 10:52:11 +0530

    Add files via upload

---
+++ Finding Square Roots.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,r;
+        scanf("%d",&n);
+        r=sqrt(n);
+        printf("%d\n",r);
+    }
+}
