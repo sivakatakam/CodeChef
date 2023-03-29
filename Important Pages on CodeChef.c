8d1b39de610605f5b2c2037c838567791e6dd839
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-03-29 23:49:10 +0530

    Add files via upload

---
+++ Important Pages on CodeChef.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a==0)
+    printf("https://www.codechef.com/practice");
+    else if(a==1&&b==0)
+    printf("https://www.codechef.com/contests");
+    else if(a==1&&b==1)
+    printf("https://discuss.codechef.com");
+}
