4c54dcdb184e82aef0319c2343940e7a4f845352
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-03-30 23:51:35 +0530

    Add files via upload

---
+++ Budget of Technex.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int x,a;
+        scanf("%d",&x);
+        a=x*100;
+        printf("%d\n",a);
+    }
+}
