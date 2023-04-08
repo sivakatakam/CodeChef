9fa4e0267a909edabc469746d5bf39077a0c2722
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-08 12:09:10 +0530

    Add files via upload

---
+++ Add Two Numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0)
+        {
+            int a,b,c;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            c=a+b;
+            System.out.println(c);
+            t--;
+        }
+    }
+}
