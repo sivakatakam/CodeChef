8d17c843cc4e79055129ef2fd98c073c94f00f72
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-09 06:10:36 +0530

    Add files via upload

---
+++ Squats.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0)
+        {
+            int x,s;
+            x=sc.nextInt();
+            s=x*15;
+            System.out.println(s);
+            t--;
+        }
+    }
+}
