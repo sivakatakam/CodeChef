fd7ec42b86e79b1af331997c1a4596e3c9c84688
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-11 18:59:26 +0530

    Add files via upload

---
+++ How many unattempted problems.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,s;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        s=a-b;
+        System.out.println(s);
+    }
+}
