afcf96f459e4e1e5b30cf52d600068f207c4119d
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-13 20:13:08 +0530

    Add files via upload

---
+++ Best of Two.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t>0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(Math.max(a,b));
+            t--;
+        }
+    }
+}
