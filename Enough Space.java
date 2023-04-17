6eec4cd0320d1ad7b4af58230ddca8a44951acfa
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-17 19:02:59 +0530

    Add files via upload

---
+++ Enough Space.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(n>=x+2*y)
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
