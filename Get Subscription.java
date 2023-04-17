014e4fe8c686ee1bc029b75b315d1f2001ead13d
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-17 19:02:29 +0530

    Add files via upload

---
+++ Get Subscription.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int x=sc.nextInt();
+            if(x>30)
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
