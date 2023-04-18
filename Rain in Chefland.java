863af45322f19eb875c1bcf218103e432fdbf140
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-18 06:15:25 +0530

    Add files via upload

---
+++ Rain in Chefland.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int x=sc.nextInt();
+            if(x<3)
+            System.out.println("LIGHT");
+            else if(x>=3&&x<7)
+            System.out.println("MODERATE");
+            else
+            System.out.println("HEAVY");
+        }
+    }
+}
