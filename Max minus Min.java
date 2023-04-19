aed0e86c06f104f4964b62164d4cf59d3cda0ec4
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-19 19:43:21 +0530

    Add files via upload

---
+++ Max minus Min.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a,b,c;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            c=sc.nextInt();
+            System.out.println(c-a);
+        }
+    }
+}
