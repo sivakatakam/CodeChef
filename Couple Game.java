2628fe0a2de91b6fdf7f3ef908948f7f673dde8c
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-20 09:55:04 +0530

    Add files via upload

---
+++ Couple Game.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int g=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(b-g);
+        }
+    }
+}
