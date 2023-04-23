211109061cc045019efd6c9526ed0cc740ba7c49
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-24 05:24:47 +0530

    Add files via upload

---
+++ Hardest Problem Bet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        if(c<a&&c<b)
+        printf("Alice\n");
+        else if(b<a&&b<c)
+        printf("Bob\n");
+        else
+        printf("Draw\n");
+    }
+}
