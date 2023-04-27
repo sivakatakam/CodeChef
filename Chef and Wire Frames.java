a742f6c7a7f3ed60d24168a54bc3e88beeb2c2da
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-27 18:45:15 +0530

    Add files via upload

---
+++ Chef and Wire Frames.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            int x=sc.nextInt();
+            System.out.println(2*(n+m)*x);
+        }
+    }
+}
