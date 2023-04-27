544e0490876c80b104d5e8f5f6e09c268d7d1498
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-27 18:44:11 +0530

    Add files via upload

---
+++ Email Reminders.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int u=sc.nextInt();
+        System.out.println(n-u);
+    }
+}
