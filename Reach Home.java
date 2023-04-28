040cf87056117fc0962f814a9ebb9cf56c62e9f7
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-29 01:18:46 +0530

    Add files via upload

---
+++ Reach Home.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if(5*x>=y)
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
