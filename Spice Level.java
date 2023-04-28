fea008671f1e86c1814d62a29c6f613b8415d6dd
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-04-29 01:17:47 +0530

    Add files via upload

---
+++ Spice Level.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int x=sc.nextInt();
+            if(x<4)
+            System.out.println("MILD");
+            else if(x>=4&&x<7)
+            System.out.println("MEDIUM");
+            else
+            System.out.println("HOT");
+        }
+    }
+}
