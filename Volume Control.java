936014192a536658eef6ddffa95fd7175e257b7f
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-02 20:12:39 +0530

    Add files via upload

---
+++ Volume Control.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int s=Math.abs(x-y);
+            System.out.println(s);
+        }
+    }
+}
