0fd21665908613cbd0571cad484f7fd7a8849adc
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-15 23:26:46 +0530

    Add files via upload

---
+++ Parliament.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            if(x>=(n/2.0))
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
