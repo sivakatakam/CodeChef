3283624cdcd3b1ea155327ea4613f334c98d2afe
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-24 23:16:29 +0530

    Add files via upload

---
+++ Learning SQL.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r,c,e,a;
+        r=sc.nextInt();
+        c=sc.nextInt();
+        e=sc.nextInt();
+        a=(r+e)*c;
+        System.out.println(a);
+    }
+}
