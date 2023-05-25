b7501e4ad0bd1a3e21d688bb1627c0c3a27d82e4
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-25 11:28:33 +0530

    Add files via upload

---
+++ Parking Charges.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y,h,a;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        h=sc.nextInt();
+        a=x+(h-1)*y;
+        System.out.println(a);
+    }
+}
