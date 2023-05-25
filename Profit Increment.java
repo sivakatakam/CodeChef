29c1676eddf5a6ed20f7d251785862c0540fa570
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-25 11:15:35 +0530

    Add files via upload

---
+++ Profit Increment.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=b+(a/10);
+            System.out.println(c);
+        }
+        
+    }
+}
