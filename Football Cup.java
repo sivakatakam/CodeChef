5c9265738e65b0a545c4eb4375178d27581b62b0
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-25 11:02:14 +0530

    Add files via upload

---
+++ Football Cup.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if(a==b&&a!=0)
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
