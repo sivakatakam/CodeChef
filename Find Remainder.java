1bd97d3022c9db7f495cd9158bcce7699fd0b178
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-26 10:01:32 +0530

    Add files via upload

---
+++ Find Remainder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(a%b);
+        }
+    }
+}
