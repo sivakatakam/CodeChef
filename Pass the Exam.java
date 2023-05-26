8842b4349939d525047b6ad7496f326446fff45b
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-26 10:02:13 +0530

    Add files via upload

---
+++ Pass the Exam.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a+b+c>=100&&a>=10&&b>=10&&c>=10)
+            System.out.println("PASS");
+            else
+            System.out.println("FAIL");
+        }
+    }
+}
