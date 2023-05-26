5e46f672f0942c65437a1d4677d6e8e1caa4a865
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-26 10:01:12 +0530

    Add files via upload

---
+++ Fill the Bucket.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int x,y,a;
+            x=sc.nextInt();
+            y=sc.nextInt();
+            a=x-y;
+            System.out.println(a);
+        }
+    }
+}
