d40cf7c2486f7e3ec8a8d63e0f698b2c6d43bfe2
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-27 11:44:26 +0530

    Add files via upload

---
+++ Scalene Triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            if(a!=b&&b!=c&&a!=c)
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
