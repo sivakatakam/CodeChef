074159b7012eb5800c5671ad88513a0be36b6785
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-28 11:11:48 +0530

    Add files via upload

---
+++ AI Analysing Code.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n<1001)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+    }
+}
