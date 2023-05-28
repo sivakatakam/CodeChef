de5f58a9f6b344b112d1c3b1799f80cf399084f2
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-05-29 00:40:28 +0530

    Add files via upload

---
+++ Tyre problem.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int n=sc.nextInt();
+            int m=sc.nextInt();
+            System.out.println((n*2)+(m*4));
+        }
+    }
+}
