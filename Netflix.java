06ccdf100c0fc4009ad7bb015be27a88ee591e06
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-02 07:41:33 +0530

    Add files via upload

---
+++ Netflix.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int x=sc.nextInt();
+            if((a+b)>=x||(b+c)>=x||(a+c)>=x)
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
