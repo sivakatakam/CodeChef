74ccffc5a3dda8e9a2c9056e63b1400cddd1ff83
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-04 08:37:02 +0530

    Add files via upload

---
+++ Janmansh and Coins.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            System.out.println((10*x)+(5*y));
+        }
+    }
+}
