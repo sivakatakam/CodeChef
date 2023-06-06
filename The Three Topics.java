836ad066afc8e0e5ecdc7b82231a577259c0f342
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-07 00:08:10 +0530

    Add files via upload

---
+++ The Three Topics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class COdechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int x=sc.nextInt();
+        if(x==a||x==b||x==c)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+    }
+}
