3175cf33b546f4711c154b324a87bb45d0c3eae1
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-06 23:40:41 +0530

    Add files via upload

---
+++ Chef and NextGen.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            if((x*y)>=(a*b))
+            System.out.println("YES");
+            else
+            System.out.println("NO");
+        }
+    }
+}
