25f7d57a286755437005420ef1f8bec30ec0fd12
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-10 23:51:11 +0530

    Add files via upload

---
+++ Good Program.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int n=sc.nextInt();
+            if(n%4==0)
+            System.out.println("Good");
+            else
+            System.out.println("Not Good");
+        }
+    }
+}
