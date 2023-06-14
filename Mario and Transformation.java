44e5658f0500a21eac545e16ca6bb9ec33a05830
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-14 23:32:47 +0530

    Add files via upload

---
+++ Mario and Transformation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int n=sc.nextInt();
+            if(n%3==0)
+            System.out.println("NORMAL");
+            else if(n%3==1)
+            System.out.println("HUGE");
+            else
+            System.out.println("SMALL");
+        }
+    }
+}
