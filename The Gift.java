8f4c456a2188c4b8fabc7c8c96cb6669db44e422
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-23 17:56:36 +0530

    Add files via upload

---
+++ The Gift.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        if(x+m>=n)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+    }
+}
