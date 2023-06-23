e2227d7e67f6b5b7afd4fa3768e2ea2aac33d6c1
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-23 18:18:24 +0530

    Add files via upload

---
+++ Small factorials.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t --> 0)
+        {
+            int n = sc.nextInt();
+            BigInteger fact = new BigInteger("1");
+            for(int i = 2; i <= n; i++)
+            fact = fact.multiply(BigInteger.valueOf(i));
+            System.out.println(fact);
+        }
+	}
+}
