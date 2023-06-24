d3e8c1fdaec9f8cf620837442e651f55f8ca8e70
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-24 21:05:43 +0530

    Add files via upload

---
+++ Finding Square Roots.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-- >0)
+        {
+            int n=sc.nextInt();
+            int s=(int)Math.sqrt(n);
+            System.out.println(s);
+        }
+    }
+}
