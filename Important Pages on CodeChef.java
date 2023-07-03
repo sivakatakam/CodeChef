43602d69443a577c4520c2b79d425d3eb1da49f2
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-07-03 20:46:11 +0530

    Add files via upload

---
+++ Important Pages on CodeChef.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a==0)
+        System.out.println("https://www.codechef.com/practice");
+        else if(b==0)
+        System.out.println("https://www.codechef.com/contests");
+        else
+        System.out.println("https://discuss.codechef.com");
+    }
+}
