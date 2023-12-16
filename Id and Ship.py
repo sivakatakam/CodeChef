ca82d9cf5aaa6dabae305ec6d8eed08620046ceb
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-12-16 15:20:41 +0530

    Add files via upload

---
+++ Id and Ship.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    s = input()
+    if s == 'b' or s == 'B':
+        print("BattleShip")
+    elif s == 'c' or s == 'C':
+        print("Cruiser")
+    elif s == 'd' or s == 'D':
+        print("Destroyer")
+    elif s == 'f' or s == 'F':
+        print("Frigate")
