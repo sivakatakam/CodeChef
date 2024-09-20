6c8f057cb9402941c1a1fac2dbc884f4be5015a4
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2024-09-20 23:55:21 +0530

    Add files via upload

---
+++ Gold Coins 101.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int q, w, z, x;
+    cin >> z >> x >> q >> w;
+    if (q > w)
+    {
+        cout << z;
+    }
+    else
+    {
+        cout << x;
+    }
+}
