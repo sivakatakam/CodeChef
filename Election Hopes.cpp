c2c5c7c846244a2b056498313eb0e0171be7a53f
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2024-09-20 23:55:39 +0530

    Add files via upload

---
+++ Election Hopes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int q, w;
+    cin >> q >> w;
+    if (q >= 2 * w)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
